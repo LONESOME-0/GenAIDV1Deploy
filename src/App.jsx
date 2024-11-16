1178a22e2b7b2b950424c731c94bf8079fbc27db
kumphon <kajohnponfff@gmail.com>
2024-11-16 22:43:48 +0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import Category from './page/Category'
 import Cart from './page/Cart'
 import ProductDetail from './page/ProductDetail'
 import OrderHistory from './page/OrderHistory'
+import Search from './page/Search'
 function App() {
   return (
     <>
-    <Cart />
+    <Search />
     </>
 
     
