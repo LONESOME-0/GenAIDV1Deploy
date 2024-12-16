71247c3981d7a2b577dfadd942f3254a68d213ad
Phai <phai.339@gmail.com>
2024-12-16 16:42:42 +0700

    updatehome

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
   return (
     <>
       <Nav logo search />
-      <div className="text-lg p-3 "></div>
+     
       <div className="my-16 p-3  lg:my-40">
         <p className="text-3xl px-5 pb-2">หมวดหมู่สินค้า</p>
         <div className=" no-scrollbar  flex items-center overflow-x-scroll snap-x snap-mandatory mx-4 space-x-7">
