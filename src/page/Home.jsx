93c8ea01fdfcaebf61ea78bd9159f9d6379b3a7c
Phai <phai.339@gmail.com>
2024-12-16 17:10:22 +0700

    updatehome

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -6,39 +6,31 @@ import CardProduct from "../components/Product/CardProduct";
 import { Link } from "react-router-dom";
 import { CategoryContext } from "../context/CategoryProvider";
 import { ProductContext } from "../context/ProductProvider";
-import About from "../components/footer/about";
-
+import About from "../components/footer/45"
 const Home = () => {
   const { category } = useContext(CategoryContext);
+  //console.log("cate1",category);
   const { product, loading } = useContext(ProductContext);
-
-  if (loading) {
-    return <p>Loading products...</p>;
-  }
+  //console.log("Home",product)
+  // if (loading) {
+  //   return <p>Loading products...</p>;
+  // }
 
   return (
     <>
       <Nav logo search />
-      <div className="flex bg-ga-primary w-full h-72 mt-28 items-center">
-        <img
-          src="/img/CartmainGenAID.png"
-          alt="cart"
-          className="flex h-[240px] ml-28"
-        />
-        <div className="text-white text-6xl">
-          ร้านขายยาคุณภาพดีต้องมีแต่ยาดีๆ
-        </div>
-        <div className="absolute left-[54%] text-white text-6xl pt-36">
-          แต่ยาดีๆ ไม่ได้อยู่ในร้านเรา
-        </div>
-      </div>
-
-      <div className="my-8 p-3">
+     
+      <div className="my-16 p-3  lg:my-40">
         <p className="text-3xl px-5 pb-2">หมวดหมู่สินค้า</p>
-        <div className="no-scrollbar flex items-center overflow-x-scroll snap-x snap-mandatory mx-4 space-x-7">
-          {category?.map((cat, index) => (
-            <CardCategry key={index} data={cat} />
-          ))}
+        <div className=" no-scrollbar  flex items-center overflow-x-scroll snap-x snap-mandatory mx-4 space-x-7">
+          <CardCategry /> <CardCategry /> <CardCategry /> <CardCategry />{" "}
+          <CardCategry />
+          <CardCategry />
+          <CardCategry />
+          <CardCategry />
+          <CardCategry />
+          <CardCategry />
+          <CardCategry />
         </div>
         <div className="grid grid-flow-col justify-items-stretch ">
           <section id="bestselling">
@@ -61,17 +53,18 @@ const Home = () => {
               <CardProduct key={product.id} product={product} />
             ))}
         </div>
-     
+      </div>
       <section id="discounted">
         <p>สินค้าลดราคา</p>
       </section>
-        <div id="about">
-          <p className="text-center text-3xl">เกี่ยวกับเรา</p>
-          <hr />
-          <About />
-        </div>
-        <NavMobile />
+
+      <div id="about ">
+        <p className=" text-center text-3xl" >เกี่ยวกับเรา</p>
+        <hr/>
+        <About   />
       </div>
+      <NavMobile />
+      
     </>
   );
 };
